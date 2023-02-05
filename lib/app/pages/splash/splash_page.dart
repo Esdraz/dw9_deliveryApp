cc4b18bf44d6df83204b20537f9c29f8417ee90a
Francisco <fsousa.jpa@gmail.com>
2023-02-04 23:09:15 -0300

    add: core da aplicacao e extensions

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,3 +1,5 @@
+import 'package:dw9_delivery_app/app/core/config/env/env.dart';
+import 'package:dw9_delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
@@ -16,8 +18,24 @@ class SplashPage extends StatelessWidget {
           DeliveryButton(
             width: 200,
             height: 200,
-            label: 'teste label',
+            label: Env.i['backend_base_url'] ?? '',
             onPressed: () {},
+          ),
+          Text(MediaQuery.of(context).size.width.toString()),
+          Text(context.screenWidth.toString()),
+          Row(
+            children: [
+              Container(
+                color: Colors.red,
+                width: context.percentWidth(.5),
+                height: 200,
+              ),
+              Container(
+                color: Colors.blue,
+                width: MediaQuery.of(context).size.width * .5,
+                height: 200,
+              ),
+            ],
           ),
           TextFormField(
             decoration: InputDecoration(labelText: 'Digite aqui'),
