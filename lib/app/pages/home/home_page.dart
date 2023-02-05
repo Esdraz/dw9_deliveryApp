d7ab2389df1f0e64e4b28adb15d53c00f5d9c21a
Francisco <fsousa.jpa@gmail.com>
2023-02-05 18:37:59 -0300

    fix: chamada product tile

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:dw9_delivery_app/app/models/product_model.dart';
-import 'package:dw9_delivery_app/app/pages/home/widgets/delivery_produtct_tile.dart';
+import 'package:dw9_delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -16,7 +16,7 @@ class HomePage extends StatelessWidget {
             child: ListView.builder(
               itemCount: 5,
               itemBuilder: (context, index) {
-                return DeliveryProdutctTile(
+                return DeliveryProductTile(
                   product: ProductModel(
                       id: 0,
                       name: 'Lanche X',
