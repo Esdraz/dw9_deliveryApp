87b494534664a210937554d9cf76f64b6484b99b
Francisco <fsousa.jpa@gmail.com>
2023-02-11 22:47:48 -0300

    add: revisao

--- lib/app/dw9_delivey_app.dart
+++ lib/app/dw9_delivey_app.dart
@@ -1,12 +1,11 @@
 import 'package:dw9_delivery_app/app/core/provider/application_binding.dart';
 import 'package:dw9_delivery_app/app/core/ui/styles/theme/theme_config.dart';
 import 'package:dw9_delivery_app/app/pages/auth/login/login_page.dart';
+import 'package:dw9_delivery_app/app/pages/auth/register/register_router.dart';
 import 'package:dw9_delivery_app/app/pages/home/home_router.dart';
 import 'package:dw9_delivery_app/app/pages/product_detail/product_detail_router.dart';
 import 'package:dw9_delivery_app/app/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
-
-import 'pages/auth/register/register_router.dart';
 
 class Dw9DeliveyApp extends StatelessWidget {
   const Dw9DeliveyApp({super.key});
